2191a15a8d622df1f0cd5a88e382ee70324c9180
rahulaunni <rahulaunni@gmail.com>
2017-08-10 23:20:00 +0530

    code removed for http to https

--- app.js
+++ app.js
@@ -37,9 +37,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
-
-
-
+//redirect all http to https
+// app.use(function(req, res, next) {
+//     if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'http')) {
+//         console.log("redirecting to https");
+//         res.redirect('https://' + req.get('Host') + req.url);
+//     }
+//     else
+//         next();
+// });
 
 // passport config
 var Account = require('./models/account');
