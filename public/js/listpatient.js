8ef98e17bbc68d652b7629de69aae5bb8cb6b580
rahulaunni <rahulaunni@gmail.com>
2017-08-15 17:00:22 +0530

    search option added

--- public/js/listpatient.js
+++ public/js/listpatient.js
@@ -23,3 +23,23 @@ $(document).on("click","#middlebar .viewpatient",function(){
   $( ".middlebar" ).html( data );
 });
 });
+
+$(document).on("click","#middlebar #search",function(){
+var search_query=$( "input[name='search_query']" ).val();
+var query=search_query.toLowerCase();
+        $('#middlebar .patlist .n1 .spname').each(function(){
+             var $this = $(this);
+             // console.log($this.text().toLowerCase().indexOf(query));
+             var pname=$this.text().toLowerCase();
+             console.log(pname.indexOf(query));
+             if(pname.indexOf(query) !== -1)
+             {
+                 $this.closest('#middlebar .patlist').fadeIn();
+             }
+            else 
+            	{
+            		$this.closest('#middlebar .patlist').fadeOut();
+            	}
+        });
+
+});
