002ce6b7783a1d85fa3b3d76884ff11a3ff5bad4
rahulaunni <rahulaunni@gmail.com>
2017-09-18 10:21:57 +0530

    added notification after adding bed and patient

--- public/js/addbed.js
+++ public/js/addbed.js
@@ -26,7 +26,57 @@ $("#register").submit(function(e){
 					        contentType: 'application/json',
 	                        url: '/addbed',						
 	                        success: function(data) {
-	                            window.location='/';
+	                        	$.notify({
+	                        		// options
+	                        		icon: 'glyphicon glyphicon-warning-sign',
+	                        		title: 'Bed Added',
+	                        		message: 'The bed details are added to database',
+	                        		url: '/',
+	                        		target: '_self'
+	                        	},{
+	                        		// settings
+	                        		element: 'body',
+	                        		position: null,
+	                        		type: "info",
+	                        		allow_dismiss: true,
+	                        		newest_on_top: true,
+	                        		showProgressbar: false,
+	                        		placement: {
+	                        			from: "top",
+	                        			align: "right"
+	                        		},
+	                        		offset: 20,
+	                        		spacing: 10,
+	                        		z_index: 1031,
+	                        		delay: 5000,
+	                        		timer: 1000,
+	                        		url_target: '_blank',
+	                        		mouse_over: null,
+	                        		animate: {
+	                        			enter: 'animated fadeInDown',
+	                        			exit: 'animated fadeOutUp'
+	                        		},
+	                        		onShow: null,
+	                        		onShown: null,
+	                        		onClose: null,
+	                        		onClosed: null,
+	                        		icon_type: 'class',
+	                        		template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+	                        			'<button type="button" aria-hidden="true" class="close" id="notclose" data-notify="dismiss">×</button>' +
+	                        			'<span data-notify="icon"></span> ' +
+	                        			'<span data-notify="title">{1}</span><br> ' +
+	                        			'<span data-notify="message"><strong><b>{2}</b></strong></span>' +
+	                        			'<div class="progress" data-notify="progressbar">' +
+	                        				'<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+	                        			'</div>' +
+	                        			'<a href="{3}" target="{4}" data-notify="url"></a>' +
+	                        		'</div>' 
+	                        	});
+	                        	$('.leftbar .addbed').removeClass("activem");
+	                        	$('.leftbar .iconhome').addClass("activem");
+	                        	$('#middlebar').load("/home");
+	                        	// window.location='/?notify=ba';
+
 	                        }
 	                    });
 		return false;
